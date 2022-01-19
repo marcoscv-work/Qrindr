ce559b4c615a1bc3f78b4fe2f5dfb1b60b02c068
Marcos Castro <marcoscv@gmail.com>
2022-01-19 22:34:24 +0100

    New version test

--- client/src/components/navigation/NavigationDesktop.tsx
+++ client/src/components/navigation/NavigationDesktop.tsx
@@ -23,64 +23,24 @@ class NavigationDesktop extends Component {
                 fa: faGamepad,
                 css: 'text-yellow-500',
             }
-        },
-        {
-            name: 'About',
-            location: '/about',
-            icon: {
-                fa: faInfoCircle,
-                css: 'text-blue-300',
-            }
         }
-    ];
-
-    navExternal = [
-        {
-            name: 'Discord',
-            location: 'https://discord.gg/BAEkm58',
-            icon: {
-                fa: faDiscord,
-                css: 'text-indigo-400',
-            }
-        },
-        {
-            name: 'GitHub',
-            location: 'https://github.com/CameronCT/Qrindr',
-            external: true,
-            icon: {
-                fa: faGithub,
-                css: 'text-gray-200',
-            }
-        },
-        {
-            name: 'Donate',
-            location: 'https://streamlabs.com/gnikgg',
-            icon: {
-                fa: faDollarSign,
-                css: 'text-orange-400',
-            }
-        },
     ];
 
     render() {
         return (
             <div>
                 <div className={"hidden lg:block"}>
-                    <div className="flex flex-wrap px-5" style={{ background: 'rgba(0, 0, 0, 0.3)' }}>
-                        <div className="w-24 mr-auto py-2">
+                    <div className="flex flex-wrap px-5" style={{ background: '#780303' }}>
+                        <div className="mr-auto py-2">
                             <Link to={"/"} className="text-2xl text-white font-bold">
                                 Q<span className={"hidden lg:inline"}>rindr</span>
                             </Link>
+                            <small className='text-white'> for Keen Con League</small>
                         </div>
                         {this.navOptions.map((row, key:number) => (
                             <Link key={key} to={row.location} className="animation-short w-auto px-5 py-4 flex justify-center font-semibold text-sm text-white hover:text-red-400 uppercase hover:bg-black hover:bg-opacity-25">
                                 {row.name}
                             </Link>
-                        ))}
-                        {this.navExternal.map((row, key: number) => (
-                            <a key={key} href={row.location} className="animation-short w-auto px-5 py-4 flex justify-center font-semibold text-sm text-white hover:text-red-400 uppercase hover:bg-black hover:bg-opacity-25">
-                                {row.name}
-                            </a>
                         ))}
                     </div>
                 </div>
@@ -95,16 +55,6 @@ class NavigationDesktop extends Component {
                                     </div>
                                 </div>
                             </Link>
-                        ))}
-                        {this.navExternal.map((row, key: number) => (
-                            <a key={key} href={row.location} className="animation-short w-1/6 py-2 text-xs uppercase hover:bg-black hover:bg-opacity-25">
-                                <div className={"text-center"}>
-                                    <FontAwesomeIcon icon={row.icon.fa} className={`text-xl ${row.icon.css}`} />
-                                    <div className={"text-white font-semibold"}>
-                                        {row.name}
-                                    </div>
-                                </div>
-                            </a>
                         ))}
                     </div>
                 </div>
--- client/src/components/pages/Home/Home.tsx
+++ client/src/components/pages/Home/Home.tsx
@@ -67,45 +67,9 @@ class Home extends Component {
         return (
             <div className={"w-full lg:w-2/3 mx-auto"}>
                 <div className={"content mx-2 my-6"}>
-                    <div style={{ backgroundImage: "url('assets/bg_1.png')", backgroundSize: 'cover' }} className={"p-16 rounded shadow-md"}>
-                        <div className={"text-4xl text-white font-bold"}>Qrindr - the <span className={"text-red-500"}>best</span> match creation tool</div>
-                        <div className={"text-gray-300 text-base"}>
-                            <div>We have created over <span className={"font-semibold"}>{statistics.totalMatches}</span> matches for esports players and teams.</div>
-                        </div>
+                    <div style={{ backgroundImage: "url('https://www.keencon.party/wp-content/uploads/2021/12/Keen-con-League.png')", backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center center' }} className={"p-16 rounded shadow-md"}>
                     </div>
-                </div>
-                {GitHub && GitHub.SHA && (
-                    <div className="border-l-2 border-indigo-600 bg-black-15 text-white py-2 px-4 mx-2 my-6">
-                        <FontAwesomeIcon icon={faGithub} /> <span className={"font-semibold"}>Latest Commit: </span> <a href={GitHub.URL} target={"_blank"} rel="noopener noreferrer">{GitHub.SHA}</a> on {GitHub.DATE}
-                    </div>
-                )}
-                <div className={"content my-6"}>
-                    <div className={"text-xl mx-2 text-white font-semibold tracking-wide pb-3 mb-3 border-b border-gray-800"}>Supported Games</div>
-                    <div className="flex flex-wrap mt-2">
-                        {this.supported.map((item) => (
-                            <div key={item.key} className={"w-1/3 px-2"}>
-                                <a key={item.key} href={item.url}>
-                                    <img src={`assets/${item.key}.png`} alt={item.key} className={"w-full h-auto rounded shadow-md border border-transparent animation-long hover:border-gray-800 hover:border-opacity-75"} />
-                                </a>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-
-                <div className={"content my-6"}>
-                    <div className={"text-xl mx-2 text-white font-semibold tracking-wide pb-3 mb-3 border-b border-gray-800"}>Recent Matches</div>
-                    <div className="flex flex-wrap pt-4 md:pt-0">
-                        {matches.map((row: any, key: number) => (
-                            <Link key={key} to={`/match/${row.matchHash}`} className="w-full lg:w-1/2 xl:w-1/3 pb-2 md:p-2">
-                                <div className="animation-long p-3 text-white text-base border-l-2 bg-black-15 hover:bg-black hover:bg-opacity-25 border-orange-600 text-center">
-                                    <div>
-                                        <span className="font-semibold">{row.matchPlayerOne}</span> vs <span className="font-semibold">{row.matchPlayerTwo}</span>
-                                    </div>
-                                    <div className="text-gray-500 text-xs">{row.matchConfig}</div>
-                                </div>
-                            </Link>
-                        ))}
-                    </div>
+                    <div className={"text-center text-white font-bold pt-10"}>Qrindr - the <span className={"text-red-500"}>best</span> match creation tool adapted to Keen Con League </div>
                 </div>
             </div>
         )
